9528617c53f3edf5802c0692ad8e1da15cecf692
jman13378 <garconiamail@gmail.com>
2025-03-21 22:26:39 -0400

    Initial commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <ion-header :translucent="true">
+        <navbar></navbar>
+    </ion-header>
+
+</template>
+<script setup lang="ts">
+
+import { computed, ref } from "vue";
+import {
+    IonPage,
+    IonIcon,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonSelectOption,
+    IonMenuButton,
+    IonButton,
+    IonContent,
+    IonList,
+    IonItem,
+    IonTitle,
+    IonRow,
+    IonCol,
+    IonInput,
+    IonToast,
+    IonSelect,
+    IonRadioGroup,
+    IonRadio
+} from "@ionic/vue";
+
+
+</script>
+<script lang="ts">
+import * as outlines from 'ionicons/icons';
+
+export default {
+    name: 'ToolBar',
+}
+</script>
